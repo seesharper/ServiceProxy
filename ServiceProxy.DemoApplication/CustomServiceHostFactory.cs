4be9d7f91cc0ddc7660ec74428cf353e1b0e1281
Bernhard Richter <bernhard.richter@gmail.com>
2011-09-08 01:26:48 +0200

    Initial commit

---
+++ ServiceProxy.DemoApplication/CustomServiceHostFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using ServiceProxy.SampleServices;
+
+namespace ServiceProxy.DemoApplication
+{
+    public class CustomServiceHostFactory : ServiceHostFactory
+    {
+        private static readonly ServiceProxyFactory ServiceProxyFactory = new ServiceProxyFactory();
+        
+        protected override ServiceHost CreateServiceHost(Type serviceType, Uri[] baseAddresses)
+        {
+            Type serviceProxy = ServiceProxyFactory.GetProxyType(serviceType, () => new SampleService(new Logger()));
+            var serviceHost = new ServiceHost(serviceProxy,baseAddresses);
+            serviceHost.Description.Name = serviceType.Name;
+            return serviceHost;
+        }
+    }
+}
---
+++ ServiceProxy.SampleServices/SampleServices.cs
@@ -0,0 +1,37 @@
+﻿using System.ServiceModel;
+
+namespace ServiceProxy.SampleServices
+{
+    [ServiceContract]
+    public interface ISampleService
+    {
+        [OperationContract]
+        int Calculate(int value1, int value2);
+    }
+
+    public class SampleService : ISampleService
+    {
+        public SampleService(ILogger logger)
+        {
+        }
+
+        public int Calculate(int value1, int value2)
+        {
+            return value1 + value2;
+        }
+    }
+
+
+    public interface ILogger
+    {
+        void Log(string message);
+    }
+
+    public class Logger : ILogger
+    {
+        public void Log(string message)
+        {
+            //Perform some logging
+        }
+    }
+}
